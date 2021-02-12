754416631faf8b1ee4abbb4bf974e66a5221a9dd
Bob den Os <bobdenos@gmail.com>
2021-02-12 23:40:30 +0100

    Add database submodule from branch

--- ci/git.js
+++ ci/git.js
@@ -4,12 +4,12 @@ const exec = util.promisify(require('child_process').exec)
 
 const push = async function (dir) {
   const options = {
-    cwd: path.resolve(__dirname, '..', dir)
+    cwd: path.resolve(__dirname, '..', dir || '.')
   }
-  dir = dir || 'scoresharp.github.io.git'
+  dir = dir || 'main'
   let ret = await exec('git add --all', options)
   ret = await exec(`git -c 'user.name=CI' -c 'user.email=CI@github.com' commit -m 'Push update to ${dir} at ${Date.now()}'`, options)
-  ret = await exec(`git push 'https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/scoresharp/scoresharp.github.io.git' HEAD:test`)
+  ret = await exec(`git push 'https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/scoresharp/scoresharp.github.io.git' HEAD:${dir}`, options)
   console.log(ret.stdout)
   console.error(ret.stderr)
 }
