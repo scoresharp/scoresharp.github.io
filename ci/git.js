b545d283188c45ed37f80d2c9224b1856a488357
Bob den Os <bobdenos@gmail.com>
2021-02-12 22:21:36 +0100

    Adjust exec API to v14

--- ci/git.js
+++ ci/git.js
@@ -1,27 +1,15 @@
 const path = require('path')
-const childProcess = require('child_process')
-
-const exec = function (cmd, args, options) {
-  return new Promise((resolve, reject) => {
-    const proc = childProcess.exec(cmd, args, options)
-    proc.on('error', reject)
-    proc.on('exit', code => {
-      if (code > 0) {
-        return reject(new Error(`Failed to execute command ${cmd}, because it returned with code ${code}`))
-      }
-      resolve()
-    })
-  })
-}
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 const push = async function (dir, token) {
   const options = {
     cwd: path.resolve(__dirname, '..', dir)
   }
   dir = dir || 'scoresharp.github.io.git'
-  await exec('git', ['add', '--all'], options)
-  await exec('git', ['commit', '-m', `Push update to ${dir} at ${Date.now()}`], options)
-  await exec('git', ['push', `https://${token}@github.com/scoresharp/${dir}.git`], options)
+  await exec('git add --all', options)
+  await exec(`git commit -m Push update to ${dir} at ${Date.now()}`, options)
+  await exec(`git push https://${token}@github.com/scoresharp/${dir}.git`, options)
 }
 
 module.exports.push = push
