35c977f0267a6d9ea3e524a2a42bf12b637f8c92
Bob den Os <bobdenos@gmail.com>
2021-02-13 22:34:32 +0100

    Create tmp root before sub folders

--- test/tmp.js
+++ test/tmp.js
@@ -3,7 +3,23 @@ const path = require('path')
 
 const tmpRoot = path.resolve(__dirname, '../.tmp')
 
+let tmpRootProm
+const ensureTmpRoot = async function () {
+  const createTmpRoot = async () => {
+    try {
+      await fs.promises.mkdir(tmpRoot)
+    } catch (err) {
+      if (err.code !== 'EEXIST') {
+        throw err
+      }
+    }
+  }
+  tmpRootProm = tmpRootProm || createTmpRoot()
+  return tmpRootProm
+}
+
 module.exports.ensure = async function (dirPath) {
+  await ensureTmpRoot()
   const dirs = dirPath.split(/[\\/]/)
   let full = '.'
   for (let dir of dirs) {
